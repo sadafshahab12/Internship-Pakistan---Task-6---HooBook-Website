3586cddbcc101f56cbac73cabbb31842ea364163
Sadaf Shahab <sadafshahab123@yahoo.com>
2025-03-18 16:42:58 +0500

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="shortcut icon"
+          href="/icon.jpg"
+          type="image/jpeg"
+        />
+      </head>
       <body className={`${livvic.className} antialiased`}>{children}</body>
     </html>
   );
